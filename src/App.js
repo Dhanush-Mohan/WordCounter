eb717f570c90407a1ee4d30ac03f490e18bcee82
Dhanush-Mohan <dhanish4777@gmail.com>
2023-10-15 00:15:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import F1 from "./Components/F1";
+function App() {
+  return (
+    <div>
+      <F1 />
+    </div>
+  );
+}
+
+export default App;
